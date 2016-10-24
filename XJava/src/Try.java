d70d653d864c561526ac1b38704842099b1afef1
Yerokhin Vladimir <kharkovvip2014@gmail.com>
2016-10-25 01:31:14 +0300

    zzz

---
+++ XJava/src/Try.java
@@ -0,0 +1,60 @@
+/**
+ * Created by x_prt_000 on 25.10.2016
+ */
+public class Try {
+    public static void main(String[] args) {
+        Cat ddd = new Cat();
+        System.out.println(ddd);
+    }
+
+
+    static class Cat {
+
+        String name;
+        String breed; //порода
+        String color;
+        int age;
+        double weight; //вес
+
+        public Cat() {
+            this.name = "no name";
+            this.breed = "no breed";
+            this.color = "no color";
+            this.age = 0;
+            this.weight = 0.0;
+        }
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        public Cat(String name, String breed, String color) {
+            this.name = name;
+            this.breed = breed;
+            this.color = color;
+        }
+
+        public Cat(String name,
+                   String breed,
+                   String color,
+                   int age,
+                   double weight) {
+            this.name = name;
+            this.breed = breed;
+            this.color = color;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return ("Cat"
+                    + " name: " + name
+                    + "\n breed: " + breed
+                    + "\n color: " + color
+                    + "\n age: " + age
+                    + "\n weight: " + weight);
+        }
+    }
+}
+
